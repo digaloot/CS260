a92082cf95a1ef6d72dc329f766a786dcf231f8c
digaloot <adam@arctechcharge.com>
2025-02-10 18:08:13 -0700

    added the footer in the app.jsx file

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+    return (
+        // <BrowserRouter>
+            <div className='body bg-white text-black'>    
+                <footer>
+                    <br/><br/>
+                    <hr />
+                    <span className="text-reset">Author's Name: Adam Gleason</span>
+                    <br />
+                    <a href="https://github.com/digaloot/CS260">GitHub</a><br /><br /><br />
+                    <div id="picture" className="love_notes_jpg"><img src="LoveNotes.jpg" alt="LoveNotes"  width="100" height="100" /><br/></div>          
+                </footer>
+            </div>
+        // </BrowserRouter>
+    );
+    
 }
